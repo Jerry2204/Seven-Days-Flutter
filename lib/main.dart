fcb5e2e8e9c6eedecc65393fcf46d4e4a18f2996
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2022-03-12 21:38:44 +0700

    first sign in

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sevendays/pages/first_signin.dart';
 import 'package:sevendays/pages/first_splash.dart';
 import 'package:sevendays/pages/first_started.dart';
 
@@ -9,7 +10,7 @@ class SevenDays extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: FirstStarted(),
+      home: FirstSignIn(),
     );
   }
 }
