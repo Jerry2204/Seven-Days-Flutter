db205ef279eecc525dad7345a94565196744e75e
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2022-03-20 23:17:19 +0700

    pricing page

--- lib/pages/first_pricing.dart
+++ lib/pages/first_pricing.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class FirstPricing extends StatelessWidget {
+class FirstPricing extends StatefulWidget {
+  @override
+  State<FirstPricing> createState() => _FirstPricingState();
+}
+
+class _FirstPricingState extends State<FirstPricing> {
+  int selectedIndex = -1;
   @override
   Widget build(BuildContext context) {
     Widget header() {
@@ -36,68 +42,91 @@ class FirstPricing extends StatelessWidget {
       );
     }
 
-    Widget option() {
-      return Container(
-        width: 315,
-        height: 100,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(60),
-          border: Border.all(
-            color: Color(0xffD9DEEA),
+    Widget option(
+      int index,
+      String imageUrl,
+      String title,
+      String description,
+      String subDescription,
+    ) {
+      return GestureDetector(
+        onTap: () {
+          setState(() {
+            selectedIndex = index;
+          });
+        },
+        child: Container(
+          width: 315,
+          height: 100,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(60),
+            border: Border.all(
+                color: selectedIndex == index
+                    ? Color(0xff6050E7)
+                    : Color(0xffD9DEEA)),
           ),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.only(
-            top: 16,
-            left: 17,
-            bottom: 23,
-          ),
-          child: Row(
-            children: [
-              Image.asset(
-                'assets/pig.png',
-                width: 65,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(
-                  top: 10,
-                  left: 7,
+          child: Padding(
+            padding: const EdgeInsets.only(
+              top: 16,
+              left: 17,
+              bottom: 23,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Image.asset(
+                  imageUrl,
+                  width: 65,
                 ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Money Security',
-                      style: GoogleFonts.poppins(
-                        color: Color(0xff191919),
-                        fontSize: 16,
-                        fontWeight: FontWeight.w500,
+                Padding(
+                  padding: const EdgeInsets.only(
+                    top: 10,
+                    left: 7,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        title,
+                        style: GoogleFonts.poppins(
+                          color: Color(0xff191919),
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                        ),
                       ),
-                    ),
-                    Row(
-                      children: [
-                        Text(
-                          'Support',
-                          style: GoogleFonts.poppins(
-                              color: Color(0xffA3A888),
-                              fontWeight: FontWeight.w300),
-                        ),
-                        SizedBox(
-                          width: 6,
-                        ),
-                        Text(
-                          '24/7',
-                          style: GoogleFonts.poppins(
-                            color: Color(0xff5343C2),
-                            fontWeight: FontWeight.w500,
+                      Row(
+                        children: [
+                          Text(
+                            description,
+                            style: GoogleFonts.poppins(
+                                color: Color(0xffA3A888),
+                                fontWeight: FontWeight.w300),
                           ),
-                        ),
-                      ],
-                    )
-                  ],
+                          SizedBox(
+                            width: 6,
+                          ),
+                          Text(
+                            subDescription,
+                            style: GoogleFonts.poppins(
+                              color: Color(0xff5343C2),
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
                 ),
-              )
-            ],
+                Padding(
+                    padding: const EdgeInsets.only(left: 40, right: 20),
+                    child: selectedIndex == index
+                        ? Image.asset(
+                            'assets/ic_check.png',
+                            width: 26,
+                          )
+                        : SizedBox()),
+              ],
+            ),
           ),
         ),
       );
@@ -111,7 +140,17 @@ class FirstPricing extends StatelessWidget {
           SizedBox(
             height: 50,
           ),
-          option(),
+          option(0, 'assets/pig.png', 'Money Security', 'support', '24/7'),
+          SizedBox(
+            height: 24,
+          ),
+          option(1, 'assets/paper_icon.png', 'Automation', 'we provide',
+              'Invoice'),
+          SizedBox(
+            height: 24,
+          ),
+          option(
+              2, 'assets/coin_icon.png', 'Balance Report', 'can up to', '10 K'),
         ],
       ),
     );
