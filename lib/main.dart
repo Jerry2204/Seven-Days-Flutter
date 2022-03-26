0cb9cae50f57b7e672e830fd6f02d30e76a98624
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2022-03-26 23:15:54 +0700

    day 7 init

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:sevendays/pages/first_rate.dart';
 import 'package:sevendays/pages/first_signin.dart';
 import 'package:sevendays/pages/first_splash.dart';
 import 'package:sevendays/pages/first_started.dart';
+import 'package:sevendays/pages/first_trade.dart';
 import 'package:sevendays/pages/second_empty.dart';
 import 'package:sevendays/pages/second_pricing.dart';
 import 'package:sevendays/pages/second_rate.dart';
@@ -19,7 +20,7 @@ class SevenDays extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SecondPricing(),
+      home: FirstTrade(),
     );
   }
 }
