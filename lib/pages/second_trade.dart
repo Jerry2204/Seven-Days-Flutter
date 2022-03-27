7db60811671f3b492394699edb272e3a75c9482c
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2022-03-27 22:45:47 +0700

    about and date section

--- lib/pages/second_trade.dart
+++ lib/pages/second_trade.dart
@@ -30,6 +30,77 @@ class SecondTrade extends StatelessWidget {
               fontWeight: FontWeight.w300,
             ),
           ),
+          SizedBox(
+            height: 26,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 24),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'About',
+                  style: GoogleFonts.poppins(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                Text(
+                  'Pantai Pandawa adalah salah satu para \nkawasan wisata di area Kuta selatan sana \nKabupaten Dekat Bandung, Bali.',
+                  style: GoogleFonts.poppins(
+                    color: Color(0xff2F323A),
+                    fontSize: 16,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+                SizedBox(
+                  height: 26,
+                ),
+                Text(
+                  'Booking Now',
+                  style: GoogleFonts.poppins(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                Row(
+                  children: [
+                    Image.asset(
+                      'assets/day_1.png',
+                      width: 80,
+                      height: 100,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Image.asset(
+                      'assets/day_2.png',
+                      width: 80,
+                      height: 100,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Image.asset(
+                      'assets/day_3.png',
+                      width: 80,
+                      height: 100,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Image.asset(
+                      'assets/day_4.png',
+                      width: 80,
+                      height: 100,
+                    ),
+                  ],
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
