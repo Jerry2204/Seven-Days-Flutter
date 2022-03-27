c31e1371502813540a55864eb5bf0021823e9947
Jerry Andrianto Pangaribuan <jerryandrianto22@gmail.com>
2022-03-27 22:32:24 +0700

    title second trade

--- lib/pages/second_trade.dart
+++ lib/pages/second_trade.dart
@@ -4,6 +4,34 @@ import 'package:google_fonts/google_fonts.dart';
 class SecondTrade extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          Image.asset('assets/cover.png'),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            'Arrina La',
+            style: GoogleFonts.poppins(
+              color: Colors.black,
+              fontSize: 26,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          SizedBox(
+            height: 2,
+          ),
+          Text(
+            'Denpasar, Bali',
+            style: GoogleFonts.poppins(
+              color: Colors.black,
+              fontSize: 16,
+              fontWeight: FontWeight.w300,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
